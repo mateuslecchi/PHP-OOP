6c0fda66ae360dc67684052b13ff63d692b4066c
Mateus Lecchi <mateus.lecchi.y@gmail.com>
2020-07-15 11:04:24 -0300

    Aula 02

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
 class Pessoa {
-    
+    public $nome;
+    public $idade;
+
+    public function Falar(){
+        echo $this->nome." de ".$this->idade." anos acabou de falar";
+    }
 }
+
+$mateus = new Pessoa();
+$mateus->nome = "Mateus Nossa Lecchi";
+$mateus->idade = 25;
+$mateus->Falar();
